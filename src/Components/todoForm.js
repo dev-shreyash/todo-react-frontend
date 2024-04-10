6af35ea4b342d90af1fbc3fcf1365b1cb767bc42
Shreyash-69 <bhosaleshreyash2@gmail.com>
2024-04-10 16:15:24 +0530

    Added todo form functionality

--- src/Components/todoForm.js
+++ src/Components/todoForm.js
@@ -1,13 +1,52 @@
 import React,{useState, useEffect, useRef} from 'react'
 
-function TodoForm() {
+function TodoForm(props) {
+    const[input, setInput] =useState(props.edit ? props.edit.value : '');
+
+    const inputRef =useRef(null)
+    useEffect(()=>{
+        inputRef.current.focus()
+    })
+    const handleChange = e =>{
+        setInput(e.target.value)
+    }
+    const handleAdd = e =>{
+        e.preventDefault()
+        props.onSubmit({
+            id:Math.floor(Math.random()*10000),
+            text: input
+        })
+      setInput('')
+    }
         
     return (
         <div>
-            <form className='todo-form'>
-              
-              
-            </form>
+            <form className='todo-form'  onSubmit={handleAdd}>
+          {props.edit ?(
+            <>
+          <input 
+          type='text' 
+          placeholder='Add todo' 
+          value={input} className='todo-input edit' 
+          onChange={handleChange} 
+          ref={inputRef}/>
+          <button className='todo-button edit'>Update Todo</button> 
+          </>
+          )
+          :
+          (
+            <>
+          <input 
+          type='text' 
+          placeholder='Add todo' 
+          value={input} className='todo-input' 
+          onChange={handleChange} 
+          ref={inputRef}/>
+          <button className='todo-button'>Add Todo</button> 
+          </>
+          )}
+          
+        </form>
         </div>
       )
     
